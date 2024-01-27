d09f6f723f227d93d8aff2abc7240335593ae25f
Erik Praxedes <116700824+ErikKarl01@users.noreply.github.com>
2024-01-27 20:19:47 -0300

    Add files via upload

---
+++ Estruturas/estruturaevetorquestao1.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Funcionario{
+	char nome[30];
+	float salario;
+	int identificador;
+	char cargo[20];
+};
+
+void cadastrafuncionario(struct Funcionario individuo[], int qt)
+{
+	int i;
+	for(i = 0; i < qt; i++)
+	{
+		printf("\n");
+		printf("Nome:\n");
+		scanf(" %[^\n]s", individuo[i].nome);
+		printf("Salario:\n");
+		scanf(" %f", &individuo[i].salario);
+		printf("Identificador:\n");
+		scanf(" %d", &individuo[i].identificador);
+		printf("Cargo:\n");
+		scanf(" %[^\n]s", individuo[i].cargo);
+		printf("\n");
+	}
+}
+
+int pesquisafuncionario(struct Funcionario individuo[], int qt)
+{
+	char pessoa[30];
+	int i;
+	printf("Digite o nome:\n");
+	scanf(" %[^\n]s", pessoa);
+	for(i = 0; i < qt; i++)
+	{
+		if(strcmp(individuo[i].nome, pessoa) == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void imprimir(struct Funcionario individuo[], int qt)
+{
+	int i;	
+	for(i = 0; i < qt; i++)
+	{
+		printf("\n");
+		printf("\n");
+		printf("Nome: %s\n", individuo[i].nome);
+		printf("Salario: %.2f\n", individuo[i].salario);
+		printf("Identificador: %d\n", individuo[i].identificador);
+		printf("Cargo: %s\n", individuo[i].cargo);
+		printf("\n");
+		printf("\n");	
+	}
+	
+}
+
+void compararsal(struct Funcionario individuo[], int qt)
+{
+	int i, indexmaior = 0, indexmenor = 0;
+	float maior = 0, menor = 0;
+	for(i = 0; i < qt; i++)
+	{
+		if(individuo[i].salario > maior)
+		{
+			maior = individuo[i].salario;
+			indexmaior = i;
+		}
+	}
+	menor += maior;
+	
+	for(i = 0; i < qt; i++)
+	{
+		if(individuo[i].salario < menor)
+		{
+			menor = individuo[i].salario;
+			indexmenor = i;
+		}
+	}
+	printf("\n");
+	printf("O menor salário foi de %s no valor de %.2f\n", individuo[indexmenor].nome, menor);
+	printf("O maior salario foi de %s no valor de %.2f\n", individuo[indexmaior].nome, maior);
+}
+
+void alterarsal(struct Funcionario *individuo)
+{
+	printf("\n");
+	printf("Quanto deseja para novo salario?\n");
+	scanf(" %f", &individuo->salario);
+	printf("\n");
+}
+
+int main(void)
+{
+	int qt;
+	printf("Escolha uma quantidade de funcionários para cadastrar: \n");
+	scanf("%d", &qt);
+	printf("\n");
+	
+	
+	struct Funcionario individuo[qt];
+	char resposta[4];
+	int i, index;
+	
+	
+	cadastrafuncionario(individuo, qt);
+	imprimir(individuo, qt);
+	
+	
+	printf("\n");
+	printf("Deseja aumentar o salario?\n");
+	scanf("%s", resposta);
+	if (strcmp(resposta, "sim")==0)
+	{
+		index = pesquisafuncionario(individuo, qt);
+		if(index == -1)
+		{
+			printf("\n");
+			printf("Pessoa não encontrada\n");
+		}
+		else
+		{
+			alterarsal(&individuo[index]);
+			imprimir(individuo, qt);
+		}
+	}
+	
+	
+	compararsal(individuo, qt);
+	
+	return 0;
+}
---
+++ Estruturas/estruturaevetorquestao2.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+
+struct Pessoa{
+	char nome[30];
+	int idade;
+	int documento;
+};
+
+int qt;
+
+void registrar (struct Pessoa *individuo) //ponteiro para primeiro elemento array individuo
+{
+	int i;
+	for(i = 0; i < qt; i++)
+	{
+		printf("\n");
+		printf("Nome:\n");
+		scanf(" %[^\n]s", individuo[i].nome);
+		printf("Idade:\n");
+		scanf("%d", &individuo[i].idade);//fornecer endereço do primeiro elemento
+		printf("Documento:\n");
+		scanf("%d", &individuo[i].documento);
+		printf("\n");
+	}
+}
+
+void listar(struct Pessoa individuo[])
+{
+	int i;
+	for(i = 0; i < qt; i++)
+	{
+		printf("\n");
+		printf("\n");
+		printf("Nome: %s\n", individuo[i].nome);
+		printf("Documento: %d\n", individuo[i].documento);
+		printf("Idade: %d\n", individuo[i].idade);
+		printf("\n");
+		printf("\n");
+	}	
+}
+
+int attidade(struct Pessoa *individuo)
+{
+	char pessoa[30];
+	int achou = -1, i;
+	printf("\n");
+	printf("Nome da pessoa que se deseja alterar a idade:\n");
+	scanf("%s", pessoa);
+	for(i = 0; i < 4; i++)
+	{
+		if(strcmp(individuo[i].nome, pessoa) == 0)
+		{
+			achou = i;
+			break;
+		}
+	}
+	if(achou>=0 && achou < 4)
+	{
+		printf("\n");
+		printf("Digite a nova idade: \n");
+		scanf(" %d", &individuo[achou].idade);
+		return 1;
+	}
+	else
+	{
+		printf("\n");
+		printf("Pessoa não encontrada!\n");
+		return 0;
+	}
+	
+}
+
+void compararidade(struct Pessoa individuo[])
+{
+	int maior = 0, menor = 0, i, indexmenor = 0, indexmaior = 0;
+	for(i = 0; i < qt; i++)
+	{
+		if(individuo[i].idade > maior)
+		{
+			maior = individuo[i].idade;
+			indexmaior = i;
+		}
+	}
+	menor += maior;
+	
+	for(i = 0; i < qt; i++)
+	{
+		if(individuo[i].idade < menor)
+		{
+			menor = individuo[i].idade;
+			indexmenor = i;
+		}
+	}
+	printf("\n");
+	printf("A pessoa de maior idade é %s de idade %d\n", individuo[indexmaior].nome, maior);
+	printf("A pessoa de menor idade é %s de idade %d\n", individuo[indexmenor].nome, menor);
+	printf("\n");
+}
+
+int main(void)
+{
+	printf("Digite a quantidade de pessoas: \n");
+	scanf("%d", &qt);
+	struct Pessoa individuo[qt];
+	char continuar[3];
+	
+	registrar(individuo);
+	listar(individuo);
+	while(true){
+		printf("Deseja atualizar a idade de uma pessoa?\n");
+		scanf("%s", continuar);
+		if(strcmp(continuar, "sim") == 0)
+		{
+			attidade(individuo);
+		}
+		else
+		{
+			listar(individuo);
+			break;
+		}	
+	}
+	
+	compararidade(individuo);
+	
+	return 0;
+}
